901e01324d25d03bc2ccaf136717dda111cd42a5
Soro Cat <sorousha326@gmail.com>
2019-05-16 10:55:09 -0700

    add changes in js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
+
+var suits = ["hearts", "diamonds", "spades", "clovers"]
+
+var numbers = ["ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "King", "Queen", "Jack"]
+
+var cards = []
+
 $(document).ready(function() {
   $('form#list').submit(function(event){
     event.preventDefault();
 
+    suits.forEach(function(suit){
+      numbers.forEach(function(number){
+        console.log(number + "of" + suit)
+         cards.push(number + "of" + suit)
+         $('#output').append('<li>'+ number + "of" + suit +'</li>')
 
-    var input = $("#Card").val()
-    var inputArray = input.split(" ")
-    inputArray.sort()
-    var newArray = inputArray.map(function(item) {
-      if (item.length >=3) {
-        return item.toUpperCase()
-      }
+      });
     });
-
-    var suit = ['ace, hearts, diamonds, spades, clovers']
-    console.log('suit: ',suit);
-    // expected output: Array ['ace, hearts, diamonds, spades, clovers']
-
-    var 
+  });
+});
